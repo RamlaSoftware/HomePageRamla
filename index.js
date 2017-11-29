aa97169f2c6161e58fe1e21f54560b713f9501b7
Cristian Franco <cfrancobedoya@gmail.com>
2017-11-29 18:21:10 -0500

    DEPLOY HEROKU

---
+++ index.js
@@ -0,0 +1,9 @@
+const app = require('./app');
+const config = require('./config')
+
+
+app.listen(config.port , function (){
+    console.log(`Aplicación corriendo en htpp://localhost:${config.port}`)
+    console.log("Desarrollado por: Ramla Pro. Software");
+    console.log("Ramla Pro. Software 2017");    
+});
