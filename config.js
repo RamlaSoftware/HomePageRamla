2e527f75bf31186771258c6d203ea15a1fd602f0
Cristian Franco <cfrancobedoya@gmail.com>
2017-12-03 14:13:55 -0500

    Cambio por proyecto personal de Cristian Franco Bedoya

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  port: process.env.PORT || 3002,
+  port: process.env.PORT || 3005,
+  owner: "Ramla Software 2018",
+  developer: "Cristian Franco Bedoya"
 }
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const app = require('./app');
-const config = require('./config')
+const config = require('./config');
 
-
-app.listen(config.port , function (){
-    console.log(`Aplicación corriendo en htpp://localhost:${config.port}`)
-    console.log("Desarrollado por: Ramla Pro. Software");
-    console.log("Ramla Pro. Software 2017");
+app.listen(config.port, function(){
+  console.log(`Aplicación corriento en http://localhost:${config.port}`);
+  console.log(`Aplicación propiedad de ${config.owner}`);
+  console.log(`Desarrollado por ${config.developer}`);
+  console.log("Ramla Software 2018");
 });
